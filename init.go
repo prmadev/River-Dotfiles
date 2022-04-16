5794dcc8221e36f3d2e6c1141728fb167dac2275
Amir Alesheikh <amirhossein.alesheikh@gmail.com>
2022-04-16 09:29:57 +0430

    added wallpaper to the river folder

--- init.go
+++ init.go
@@ -170,7 +170,7 @@ func keyboardShortcuts() {
 func autorun() {
 	cmdList := []*exec.Cmd{
 		exec.Command("swww", "init"),
-		exec.Command("swww", "img", "/home/amir/Pictures/wallpapers/city.gif"),
+		exec.Command("swww", "img", "/home/amir/.config/river/city.gif"),
 		exec.Command("cfw"),
 		exec.Command("udiskie"),
 		exec.Command(
@@ -182,12 +182,13 @@ func autorun() {
 		),
 		exec.Command(
 			"dbus-update-activation-environment",
+      "SEATD_SOCK",
 			"DISPLAY",
 			"WAYLAND_DISPLAY",
 			"XDG_SESSION_TYPE",
 			"XDG_CURRENT_DESKTOP",
 		),
-		exec.Command("mako"),
+		exec.Command("wired","-r"),
 	}
 	for _, cmd := range cmdList {
 		cmdStart(cmd)
