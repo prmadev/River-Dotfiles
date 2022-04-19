520f79dbc4d6f506d28a641a95f6f24b3fd73fdc
Amir Alesheikh <amirhossein.alesheikh@gmail.com>
2022-04-19 16:07:54 +0430

    fixed an obvious bug in layout.go / moved init.go to main.go

--- layout.go
+++ layout.go
@@ -3,7 +3,7 @@ package main
 import "os/exec"
 
 func layout() {
-	cmd := exec.Command("riverctl","spawn","'rivertile", "-view-padding", "05", "-outer-padding", "05'")
+	cmd := exec.Command("riverctl", "spawn", "rivertile -view-padding 05 -outer-padding 05")
 	cmdStart(cmd)
 	allArgs := [][]string{
 		{"default-layout", "rivertile"},
